6146e80273f58a01aaf2835ad03e450cc8f93f3f
jstarczewski <jan.a.starczewski@gmail.com>
2018-09-03 10:37:15 +0200

    Renaming and creating xml files for needed activities

---
+++ RecyclerViewTutorial/app/src/main/java/com/clakestudio/pc/recyclerviewtutorial/view/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.clakestudio.pc.recyclerviewtutorial.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.clakestudio.pc.recyclerviewtutorial.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
