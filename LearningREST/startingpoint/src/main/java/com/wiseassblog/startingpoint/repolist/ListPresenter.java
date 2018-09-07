fc3df32c5b691c7aa0ae32d5a65cfb6c6666db1b
jstarczewski <jan.a.starczewski@gmail.com>
2018-09-07 12:49:45 +0200

    Building ListPresenter with Dagger2 injections

--- LearningREST/startingpoint/src/main/java/com/wiseassblog/startingpoint/repolist/ListPresenter.java
+++ LearningREST/startingpoint/src/main/java/com/wiseassblog/startingpoint/repolist/ListPresenter.java
@@ -1,8 +1,43 @@
 package com.wiseassblog.startingpoint.repolist;
 
+import com.wiseassblog.startingpoint.data.RepositoryDataSourceInterface;
+import com.wiseassblog.startingpoint.util.BaseSchedulerProvider;
+
+import javax.inject.Inject;
+
+import io.reactivex.disposables.CompositeDisposable;
+
 /**
  * Created by Jan on 9/7/2018.
  */
 
 public class ListPresenter {
+
+
+    private ViewInterface view;
+
+
+    // abstraction of our dataSource
+    private RepositoryDataSourceInterface dataSource;
+
+
+    // This helps us manage our Reactive streams
+    private CompositeDisposable disposable;
+
+    // it helps us provide our RX implementation with Schedulers
+    private BaseSchedulerProvider schedulerProvider;
+
+    @Inject
+    public ListPresenter(ViewInterface view,
+                         RepositoryDataSourceInterface dataSource,
+                         CompositeDisposable compositeDisposable,
+                         BaseSchedulerProvider schedulerProvider) {
+        this.view = view;
+        this.dataSource = dataSource;
+        this.disposable = compositeDisposable;
+        this.schedulerProvider = schedulerProvider;
+    }
+
+    
+
 }
